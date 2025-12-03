c505ee6993dc96bce6d1a0098828e19998a2314c
Emilio León <146466650+emilioleon04@users.noreply.github.com>
2025-12-03 15:16:27 -0600

    Refactor SQL inserts for categories and subcategories

--- inserts/1_categorias-subcategorias-cs.sql
+++ inserts/1_categorias-subcategorias-cs.sql
@@ -1,97 +1,79 @@
--- PASO 1: CATÁLOGOS
 BEGIN;
 
 -- 1. CATEGORÍAS
 INSERT INTO categoria (nombre_categoria) VALUES 
 ('Conciertos y Festivales'),
-('Teatro y Cultura'),
-('Deportes'),
-('Familiares'),
-('Especiales')
+('Teatro y Cultura'),        
+('Deportes'),                
+('Familiares'),             
+('Especiales')               
 ON CONFLICT DO NOTHING;
 
--- 2. SUBCATEGORÍAS (Por Lotes)
--- Subcategorías de Conciertos y Festivales
+-- 2. SUBCATEGORÍAS
 INSERT INTO subcategoria (nombre_subcategoria, categoria_id) VALUES 
-('Cabaret', (SELECT categoria_id FROM categoria WHERE nombre_categoria = 'Conciertos y Festivales' LIMIT 1)),
-('Festivales', (SELECT categoria_id FROM categoria WHERE nombre_categoria = 'Conciertos y Festivales' LIMIT 1)),
-('Rock/Metal', (SELECT categoria_id FROM categoria WHERE nombre_categoria = 'Conciertos y Festivales' LIMIT 1)),
-('Infantil', (SELECT categoria_id FROM categoria WHERE nombre_categoria = 'Conciertos y Festivales' LIMIT 1)),
-('Flamenco/Tango', (SELECT categoria_id FROM categoria WHERE nombre_categoria = 'Conciertos y Festivales' LIMIT 1)),
-('Oldies/Trova', (SELECT categoria_id FROM categoria WHERE nombre_categoria = 'Conciertos y Festivales' LIMIT 1)),
-('Urbanos', (SELECT categoria_id FROM categoria WHERE nombre_categoria = 'Conciertos y Festivales' LIMIT 1)),
-('Comedia', (SELECT categoria_id FROM categoria WHERE nombre_categoria = 'Conciertos y Festivales' LIMIT 1)),
-('Folclor/Popular', (SELECT categoria_id FROM categoria WHERE nombre_categoria = 'Conciertos y Festivales' LIMIT 1)),
-('Pop/Romantica', (SELECT categoria_id FROM categoria WHERE nombre_categoria = 'Conciertos y Festivales' LIMIT 1)),
-('Internacional', (SELECT categoria_id FROM categoria WHERE nombre_categoria = 'Conciertos y Festivales' LIMIT 1)),
-('Electronica', (SELECT categoria_id FROM categoria WHERE nombre_categoria = 'Conciertos y Festivales' LIMIT 1)),
-('Jazz/Instrumental', (SELECT categoria_id FROM categoria WHERE nombre_categoria = 'Conciertos y Festivales' LIMIT 1)),
-('Reggae', (SELECT categoria_id FROM categoria WHERE nombre_categoria = 'Conciertos y Festivales' LIMIT 1))
-ON CONFLICT DO NOTHING;
-
--- Subcategorías de Teatro y Cultura
-INSERT INTO subcategoria (nombre_subcategoria, categoria_id) VALUES 
-('Ballet/Danza', (SELECT categoria_id FROM categoria WHERE nombre_categoria = 'Teatro y Cultura' LIMIT 1)),
-('Musicales', (SELECT categoria_id FROM categoria WHERE nombre_categoria = 'Teatro y Cultura' LIMIT 1)),
-('Espectaculo', (SELECT categoria_id FROM categoria WHERE nombre_categoria = 'Teatro y Cultura' LIMIT 1)),
-('Performance', (SELECT categoria_id FROM categoria WHERE nombre_categoria = 'Teatro y Cultura' LIMIT 1)),
-('Obras de Teatro', (SELECT categoria_id FROM categoria WHERE nombre_categoria = 'Teatro y Cultura' LIMIT 1)),
-('Belleza/Moda', (SELECT categoria_id FROM categoria WHERE nombre_categoria = 'Teatro y Cultura' LIMIT 1)),
-('Exhibiciones', (SELECT categoria_id FROM categoria WHERE nombre_categoria = 'Teatro y Cultura' LIMIT 1)),
-('Voces/Coros', (SELECT categoria_id FROM categoria WHERE nombre_categoria = 'Teatro y Cultura' LIMIT 1)),
-('Drama', (SELECT categoria_id FROM categoria WHERE nombre_categoria = 'Teatro y Cultura' LIMIT 1)),
-('Festivales Culturales', (SELECT categoria_id FROM categoria WHERE nombre_categoria = 'Teatro y Cultura' LIMIT 1)),
-('Ensamble/Musica Clasica', (SELECT categoria_id FROM categoria WHERE nombre_categoria = 'Teatro y Cultura' LIMIT 1)),
-('Orquestas/Opera', (SELECT categoria_id FROM categoria WHERE nombre_categoria = 'Teatro y Cultura' LIMIT 1))
-ON CONFLICT DO NOTHING;
-
--- Subcategorías de Deportes
-INSERT INTO subcategoria (nombre_subcategoria, categoria_id) VALUES 
-('Automovilismo', (SELECT categoria_id FROM categoria WHERE nombre_categoria = 'Deportes' LIMIT 1)),
-('Toros', (SELECT categoria_id FROM categoria WHERE nombre_categoria = 'Deportes' LIMIT 1)),
-('Deportes Extremos', (SELECT categoria_id FROM categoria WHERE nombre_categoria = 'Deportes' LIMIT 1)),
-('Americano', (SELECT categoria_id FROM categoria WHERE nombre_categoria = 'Deportes' LIMIT 1)),
-('Rodeo', (SELECT categoria_id FROM categoria WHERE nombre_categoria = 'Deportes' LIMIT 1)),
-('Handball', (SELECT categoria_id FROM categoria WHERE nombre_categoria = 'Deportes' LIMIT 1)),
-('Artes Marciales Mixtas', (SELECT categoria_id FROM categoria WHERE nombre_categoria = 'Deportes' LIMIT 1)),
-('Tenis', (SELECT categoria_id FROM categoria WHERE nombre_categoria = 'Deportes' LIMIT 1)),
-('Box/Lucha Libre', (SELECT categoria_id FROM categoria WHERE nombre_categoria = 'Deportes' LIMIT 1)),
-('Beisbol', (SELECT categoria_id FROM categoria WHERE nombre_categoria = 'Deportes' LIMIT 1)),
-('Equestre', (SELECT categoria_id FROM categoria WHERE nombre_categoria = 'Deportes' LIMIT 1)),
-('Futbol', (SELECT categoria_id FROM categoria WHERE nombre_categoria = 'Deportes' LIMIT 1))
-ON CONFLICT DO NOTHING;
-
--- Subcategorías de Familiares
-INSERT INTO subcategoria (nombre_subcategoria, categoria_id) VALUES 
-('On Ice', (SELECT categoria_id FROM categoria WHERE nombre_categoria = 'Familiares' LIMIT 1)),
-('Campamentos/Tours', (SELECT categoria_id FROM categoria WHERE nombre_categoria = 'Familiares' LIMIT 1)),
-('Parque de diversiones', (SELECT categoria_id FROM categoria WHERE nombre_categoria = 'Familiares' LIMIT 1)),
-('Circos/Espectaculo Infantil', (SELECT categoria_id FROM categoria WHERE nombre_categoria = 'Familiares' LIMIT 1)),
-('Magia', (SELECT categoria_id FROM categoria WHERE nombre_categoria = 'Familiares' LIMIT 1)),
-('Espacios Infantiles', (SELECT categoria_id FROM categoria WHERE nombre_categoria = 'Familiares' LIMIT 1)),
-('Ferias/Festivales', (SELECT categoria_id FROM categoria WHERE nombre_categoria = 'Familiares' LIMIT 1)),
-('Acuarios/Parques Acuaticos', (SELECT categoria_id FROM categoria WHERE nombre_categoria = 'Familiares' LIMIT 1)),
-('Absurdo/Busqueda/Farsa', (SELECT categoria_id FROM categoria WHERE nombre_categoria = 'Familiares' LIMIT 1))
-ON CONFLICT DO NOTHING;
-
--- Subcategorías de Especiales
-INSERT INTO subcategoria (nombre_subcategoria, categoria_id) VALUES 
-('Certificados de regalo', (SELECT categoria_id FROM categoria WHERE nombre_categoria = 'Especiales' LIMIT 1)),
-('Seminarios/Conferencias', (SELECT categoria_id FROM categoria WHERE nombre_categoria = 'Especiales' LIMIT 1)),
-('Hotel', (SELECT categoria_id FROM categoria WHERE nombre_categoria = 'Especiales' LIMIT 1)),
-('Especiales', (SELECT categoria_id FROM categoria WHERE nombre_categoria = 'Especiales' LIMIT 1)),
-('Sorteos', (SELECT categoria_id FROM categoria WHERE nombre_categoria = 'Especiales' LIMIT 1)),
-('Expo/Convencion', (SELECT categoria_id FROM categoria WHERE nombre_categoria = 'Especiales' LIMIT 1)),
-('Campamentos', (SELECT categoria_id FROM categoria WHERE nombre_categoria = 'Especiales' LIMIT 1)),
-('Guias', (SELECT categoria_id FROM categoria WHERE nombre_categoria = 'Especiales' LIMIT 1)),
-('Restaurantes', (SELECT categoria_id FROM categoria WHERE nombre_categoria = 'Especiales' LIMIT 1)),
-('Paquete Comida/Cena', (SELECT categoria_id FROM categoria WHERE nombre_categoria = 'Especiales' LIMIT 1)),
-('Audiovisual/Cine', (SELECT categoria_id FROM categoria WHERE nombre_categoria = 'Especiales' LIMIT 1)),
-('Merchandise', (SELECT categoria_id FROM categoria WHERE nombre_categoria = 'Especiales' LIMIT 1)),
-('Cupones', (SELECT categoria_id FROM categoria WHERE nombre_categoria = 'Especiales' LIMIT 1)),
-('Estacionamiento', (SELECT categoria_id FROM categoria WHERE nombre_categoria = 'Especiales' LIMIT 1)),
-('Donaciones', (SELECT categoria_id FROM categoria WHERE nombre_categoria = 'Especiales' LIMIT 1)),
-('Audio Tours', (SELECT categoria_id FROM categoria WHERE nombre_categoria = 'Especiales' LIMIT 1))
+('Cabaret', 1),
+('Festivales', 1),
+('Rock/Metal', 1),
+('Infantil', 1),
+('Flamenco/Tango', 1),
+('Oldies/Trova', 1),
+('Urbanos', 1),
+('Comedia', 1),
+('Folclor/Popular', 1),
+('Pop/Romantica', 1),
+('Internacional', 1),
+('Electronica', 1),
+('Jazz/Instrumental', 1),
+('Reggae', 1),
+('Ballet/Danza', 2),
+('Musicales', 2),
+('Espectaculo', 2),
+('Performance', 2),
+('Obras de Teatro', 2),
+('Belleza/Moda', 2),
+('Exhibiciones', 2),
+('Voces/Coros', 2),
+('Drama', 2),
+('Festivales Culturales', 2),
+('Ensamble/Musica Clasica', 2),
+('Orquestas/Opera', 2),
+('Automovilismo', 3),
+('Toros', 3),
+('Deportes Extremos', 3),
+('Americano', 3),
+('Rodeo', 3),
+('Handball', 3),
+('Artes Marciales Mixtas', 3),
+('Tenis', 3),
+('Box/Lucha Libre', 3),
+('Beisbol', 3),
+('Equestre', 3),
+('Futbol', 3),
+('On Ice', 4),
+('Campamentos/Tours', 4),
+('Parque de diversiones', 4),
+('Circos/Espectaculo Infantil', 4),
+('Magia', 4),
+('Espacios Infantiles', 4),
+('Ferias/Festivales', 4),
+('Acuarios/Parques Acuaticos', 4),
+('Absurdo/Busqueda/Farsa', 4),
+('Certificados de regalo', 5),
+('Seminarios/Conferencias', 5),
+('Hotel', 5),
+('Especiales', 5),
+('Sorteos', 5),
+('Expo/Convencion', 5),
+('Campamentos', 5),
+('Guias', 5),
+('Restaurantes', 5),
+('Paquete Comida/Cena', 5),
+('Audiovisual/Cine', 5),
+('Merchandise', 5),
+('Cupones', 5),
+('Estacionamiento', 5),
+('Donaciones', 5),
+('Audio Tours', 5)
 ON CONFLICT DO NOTHING;
 
 -- 3. CARGOS
